4052ad9508ec4900c9ef63705333d0c8acbc9d28
Your Name <you@example.com>
2021-07-16 20:35:26 -0700

    add cmake install.  add  install compile on ubunt21.04

---
+++ Source/config.h
@@ -0,0 +1,12 @@
+// use static lib
+#define FREEIMAGE_LIB
+
+/* #undef SUPPORT_FMT_WEBP */
+
+#define SUPPORT_FMT_JPEG
+
+/* #undef SUPPORT_FMT_OPENEXR */
+
+#define SUPPORT_FMT_TIFF
+
+/* #undef SUPPORT_FMT_JXR */
